a9b4348fca183540ec30edd200063055558ef3f2
unknown <Chaitanya.wasnik@chistats.com>
2024-12-03 21:02:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,React, useState} from 'react';
+import axios from 'axios';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import { Grid2 } from '@mui/material';
 
+ 
 function App() {
+  const [data,setData] = useState([])
+  const url = 'https://xcountries-backend.azurewebsites.net/all'
+  useEffect(() => {
+    // Code for the side effect
+    try {
+      const fetchedData =  axios.get(url).then(res => {
+         setData(res.data);
+        console.log(res.data,"fetchData...")
+      })
+      //console.log(fetchedData,"fetchData...")
+    } catch (error) {
+      console.assertlog("Checking error...")
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+     <Grid2 container spacing={3}>
+      {data.map((item) => (
+        <Grid2 item xs={12} sm={6} md={4} key={item.id}>
+          <Card sx={{ width: 200 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={item.flag} // Use image URL from the data
+              alt={item.name}
+            />
+            <CardContent>
+              <Typography gutterBottom sx={{fontSize:"1em"}}>
+                {item.name} {/* Use title from the data */}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid2>
+      ))}
+    </Grid2>
   );
 }
 
